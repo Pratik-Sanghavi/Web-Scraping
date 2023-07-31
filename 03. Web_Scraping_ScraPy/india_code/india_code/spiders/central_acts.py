4f0c1008f6437d08457b4f5148dd5c080e7334ce
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2023-07-31 17:52:14 +0530

    Use scrapy for getting indiacode pdf details

---
+++ 03. Web_Scraping_ScraPy/india_code/india_code/spiders/central_acts.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import scrapy
+
+class CentralActsSpider(scrapy.Spider):
+    name="central_acts"
+    start_urls = [
+        "https://www.indiacode.nic.in/handle/123456789/1362/browse?page-token=1552de317077&page-token-value=0eb31a8e0107c0923155682b5424e2c7&nccharset=ABD5E664&type=ministry&order=ASC&rpp=100&submit_browse=Update"
+    ]
+    
+    def parse(self, response):
+        links = response.css('.list-group-item>a::attr(href)').getall()
+        categories = response.css('.list-group-item>a::text').getall()
+        for link, category in zip(links, categories):
+            yield response.follow(link, callback=self.parse_category, cb_kwargs={'category':category})
+    
+    def parse_category(self, response, category):
+        view_links = response.xpath('//td[@headers="t4"]/a/@href').extract()
+        for link in view_links:
+            yield {
+                'Category': category,
+                'PDF Act': response.urljoin(response.follow(link, callback=self.parse_detail, cb_kwargs={'category':category}))
+            }
+
+    def parse_detail(self, response, category):
+        details = response.css('.row>a::attr(href)').get()
+        yield details
