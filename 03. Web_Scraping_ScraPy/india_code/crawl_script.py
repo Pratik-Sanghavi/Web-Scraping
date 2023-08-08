f9daf604bdc9a1fd6149e0e231a4e7f1d2f1c38e
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2023-08-08 11:09:46 +0530

    a few file deletions

--- 03. Web_Scraping_ScraPy/india_code/crawl_script.py
+++ 03. Web_Scraping_ScraPy/india_code/crawl_script.py
@@ -1,5 +1,3 @@
-# my_crawl_script.py
-
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from india_code.spiders.central_acts import CentralActsSpider as SpiderCA
