7b27660514168d28db5d501a7b70a5f0724baac0
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2023-08-10 18:20:24 +0530

    Crawl script includes state act spider

--- 03. Web_Scraping_ScraPy/india_code/crawl_script.py
+++ 03. Web_Scraping_ScraPy/india_code/crawl_script.py
@@ -1,11 +1,13 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from india_code.spiders.central_acts import CentralActsSpider as SpiderCA
+from india_code.spiders.state_acts import StateActsSpider as SpiderSA
 
 def run_spiders():
     process = CrawlerProcess(get_project_settings())
 
     process.crawl(SpiderCA)
+    process.crawl(SpiderSA)
 
     process.start()
 
